367e49b91c3725c8b9f576d7100dbbda49e2019b
AHreJIuHA <angelinakravchenko27@gmail.com>
2024-11-07 14:30:30 +0200

    fixed Homework_1.4 (Lesson_1)

--- Lesson_1/Homework_1.4/main.js
+++ Lesson_1/Homework_1.4/main.js
@@ -1,8 +1,4 @@
 const userName = prompt('Name', 'Angelina')
-console.log(userName);
-
 const userLastName = prompt('Name', 'Olegivna')
-console.log(userLastName);
-
-const userAge = prompt('Name', '34')
-console.log(userAge);
+const userAge = prompt('Name', '33')
+console.log(userName + ' ' + userLastName + ' ' + userAge);
